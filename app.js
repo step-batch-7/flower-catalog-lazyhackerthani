30c9c47f0fa72095a94a1779f7f9ece8800ef5ad
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-01-25 15:31:42 +0530

    added server and it will give index.html

---
+++ app.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const Response = require('./lib/response');
+const CONTENT_TYPES = require('./lib/mimeTypes');
+const STATIC_FOLDER = `${__dirname}/public`;
+
+const serveFile = req => {
+  const path = `${STATIC_FOLDER}${req.url}`;
+  const stat = fs.existsSync(path) && fs.statSync(path);
+  if (!stat || !stat.isFile()) return new Response();
+  const [, extension] = path.match(/.*\.(.*)$/) || [];
+  const contentType = CONTENT_TYPES[extension];
+  const content = fs.readFileSync(path);
+  const res = new Response();
+  res.setHeader('Content-Type', contentType);
+  res.setHeader('Content-Length', content.length);
+  res.statusCode = 200;
+  res.body = content;
+  return res;
+};
+
+const findHandler = req => {
+  if (req.method === 'GET' && req.url === '/') {
+    req.url += 'index.html';
+    return serveFile;
+  }
+  if (req.method === 'GET') return serveFile;
+  return () => new Response();
+};
+const processRequest = req => {
+  const handler = findHandler(req);
+  return handler(req);
+};
+
+module.exports = { processRequest };
