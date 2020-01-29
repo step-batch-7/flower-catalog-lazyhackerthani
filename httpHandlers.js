f6e51b84eb681564a70ca9f9aafc4a575479ffde
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-01-29 19:01:23 +0530

    compleately changed to http server
    What changes you have made?
    .....
    http server is added
    net server modules are also present
    ESLINT is not added
    
    Why this change is necessary?
    ....
    
    What is the impact of the change?
    .....

---
+++ httpHandlers.js
@@ -0,0 +1,126 @@
+const fs = require('fs');
+
+const { App } = require('./httpApp');
+const { loadTemplate } = require('./lib/viewTemplate');
+
+const MIME_TYPES = {
+  txt: 'text/plain',
+  html: 'text/html',
+  css: 'text/css',
+  js: 'application/javascript',
+  json: 'application/json',
+  gif: 'image/gif',
+  jpg: 'image/jpeg',
+  pdf: 'application/pdf'
+};
+
+const serveStaticPage = function(req, res, next) {
+  const publicFolder = `${__dirname}/public`;
+  const path = req.url === '/' ? '/index.html' : req.url;
+  const absolutePath = publicFolder + path;
+  const stat = fs.existsSync(absolutePath) && fs.statSync(absolutePath);
+  if (!stat || !stat.isFile()) {
+    next();
+    return;
+  }
+  const content = fs.readFileSync(absolutePath);
+  const extension = path.split('.').pop();
+  res.setHeader('Content-Type', MIME_TYPES[extension]);
+  res.end(content);
+};
+
+const notFound = function(req, res) {
+  res.writeHead(404);
+  res.end('Not Found');
+};
+////
+
+const giveFlowerPage = (req, res, next) => {
+  const documentFolder = `${__dirname}/public/documents`;
+  const flowerName = `${req.url.slice(1)}`;
+  const description = fs.readFileSync(
+    `${documentFolder}/${flowerName}.txt`,
+    'utf8'
+  );
+  const content = loadTemplate('flower.html', { flowerName, description });
+  res.setHeader('Content-Type', MIME_TYPES.html);
+  res.end(content);
+};
+
+const giveGuestBook = function(req, res, next) {
+  const oldComments = JSON.parse(
+    fs.readFileSync('./public/documents/comments.json', 'utf8')
+  );
+  let html = '';
+  oldComments.forEach(commentDetail => {
+    html += `<div class="commentBox"> ${commentDetail.name} ( ${new Date(
+      commentDetail.time
+    )} ) : ${commentDetail.comment}</div>`;
+  });
+  const content = loadTemplate('guestBook.html', { comment: html });
+  res.setHeader('Content-Type', MIME_TYPES.html);
+  res.end(content);
+};
+
+const decodeUriText = function(encodedText) {
+  return decodeURIComponent(encodedText.replace(/\+/g, ' '));
+};
+
+const pickupParams = (query, keyValue) => {
+  const [key, value] = keyValue.split('=');
+  query[key] = decodeUriText(value);
+  return query;
+};
+
+const addComment = (req, res, next) => {
+  const oldComments = JSON.parse(
+    fs.readFileSync('./public/documents/comments.json', 'utf8')
+  );
+  const body = req.body.split('&').reduce(pickupParams, {});
+  const commentDetails = {
+    name: body.name,
+    comment: body.comment,
+    time: new Date()
+  };
+  oldComments.unshift(commentDetails);
+  fs.writeFileSync(
+    './public/documents/comments.json',
+    JSON.stringify(oldComments),
+    'utf8'
+  );
+
+  res.setHeader('location', 'guestBook');
+  res.writeHead(301);
+  res.end();
+};
+////
+const methodNotAllowed = function(req, res) {
+  res.writeHead(400, 'Method Not Allowed');
+  res.end();
+};
+
+const readBody = function(req, res, next) {
+  let data = '';
+  req.on('data', chunk => (data += chunk));
+  req.on('end', () => {
+    req.body = data;
+    next();
+  });
+};
+
+const app = new App();
+
+app.use(readBody);
+
+app.get('', serveStaticPage);
+
+app.get('Abeliophyllum', giveFlowerPage);
+app.get('Agerantum', giveFlowerPage);
+app.get('guestBook', giveGuestBook);
+app.post('guestBook', addComment);
+
+app.get('', notFound);
+app.post('', notFound);
+app.use(methodNotAllowed);
+
+module.exports = { app };
